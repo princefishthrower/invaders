3422eac58cb063801c3770e2172a38ab25376a11
Chris <frewin.christopher@gmail.com>
2018-12-28 15:25:36 -0500

    push build folder since we can't build on the weakling droplet

---
+++ build/precache-manifest.ba72adab136f5b18b6d36a3a150a6595.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "897bb352713c0a593d57e0f3ba318c56",
+    "url": "./static/media/schematic.897bb352.svg"
+  },
+  {
+    "revision": "a0ac397896d932795024dacda2043626",
+    "url": "./static/media/sample_invaders.a0ac3978.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5b56b6487f4c83fc4e68",
+    "url": "./static/js/main.5b56b648.chunk.js"
+  },
+  {
+    "revision": "1d3c68fe61771cdbad5c",
+    "url": "./static/js/1.1d3c68fe.chunk.js"
+  },
+  {
+    "revision": "5b56b6487f4c83fc4e68",
+    "url": "./static/css/main.53a0541e.chunk.css"
+  },
+  {
+    "revision": "92b8d23f2ee105a8e311fd4b062f5ceb",
+    "url": "./index.html"
+  }
+];
