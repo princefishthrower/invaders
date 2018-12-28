31650ae95c377abb6f8abb741faba2bd91cd6eee
Chris <frewin.christopher@gmail.com>
2018-12-28 18:56:58 -0500

    added logger to server

--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 const http = require('http');
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const app = express();
 let server;
+
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 app.use('/', express.static('./build'));
 
