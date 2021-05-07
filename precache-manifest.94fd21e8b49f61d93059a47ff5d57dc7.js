3d5b97a34288bd1241acd230165d8d5af16f3839
Chris Frewin <frewin.christopher@gmail.com>
2021-05-07 09:54:24 +0200

    Updates

---
+++ precache-manifest.94fd21e8b49f61d93059a47ff5d57dc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "29bbe590560726a8aafc",
+    "url": "/invaders/static/css/main.b558e987.chunk.css"
+  },
+  {
+    "revision": "29bbe590560726a8aafc",
+    "url": "/invaders/static/js/main.57eaaa74.chunk.js"
+  },
+  {
+    "revision": "5b564d8bc9b18c7661bb",
+    "url": "/invaders/static/js/runtime~main.26fdbcf1.js"
+  },
+  {
+    "revision": "549b2bbc3eac398ff6b9",
+    "url": "/invaders/static/js/2.2c99a8cf.chunk.js"
+  },
+  {
+    "revision": "897bb352713c0a593d57e0f3ba318c56",
+    "url": "/invaders/static/media/schematic.897bb352.svg"
+  },
+  {
+    "revision": "a0ac397896d932795024dacda2043626",
+    "url": "/invaders/static/media/sample_invaders.a0ac3978.svg"
+  },
+  {
+    "revision": "e78127e486577eb02f139096649e899b",
+    "url": "/invaders/index.html"
+  }
+];
